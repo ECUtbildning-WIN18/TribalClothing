09b1ca0fc9ce4bd935e306741fccabc56c8ecda8
Stefan Pettersson <trancedude79@hotmail.com>
2018-11-07 09:26:12 +0100

    Add ListView class

---
+++ TribalClothing.ProductImporter/View/ListView.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+namespace TribalClothing.ProductImporter.View
+{
+    class ListView
+    {
+        public static void Diaplay()
+        {
+            using (var context = new TribalClothingContext())
+            {
+                var productList = context.Products.ToList();
+
+                foreach (var productList in list)
+                {
+
+                }
+
+            }
+        }
+    }
+}
