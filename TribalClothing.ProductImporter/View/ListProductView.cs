4b2a42d059260c202c827b831349d1441c547111
Paula Hernandez <paula.frost@hotmail.com>
2018-11-07 10:00:25 +0100

    Add MenuView

--- TribalClothing.ProductImporter/View/ListProductView.cs
+++ TribalClothing.ProductImporter/View/ListProductView.cs
@@ -1,10 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace TribalClothing.ProductImporter.View
+﻿namespace TribalClothing.ProductImporter.View
 {
     class ListProductView
     {
+
     }
 }
--- TribalClothing.ProductImporter/View/MainMenuView.cs
+++ TribalClothing.ProductImporter/View/MainMenuView.cs
@@ -1,10 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace TribalClothing.ProductImporter.View
 {
     class MainMenuView
     {
+        public void Display()
+        {
+            var isRunning = true;
+
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine("1. List products");
+                Console.WriteLine("2. Import from CSV");
+                Console.WriteLine("3. Import from JSON");
+                Console.WriteLine("4. Clear product catalog");
+
+                var consoleKeyInfo = Console.ReadKey();
+
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.D1:
+                        
+                        break;
+                    case ConsoleKey.D2:
+                        
+                        break;
+                    case ConsoleKey.D3:
+
+                        break;
+                    case ConsoleKey.D4:
+
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (!isRunning);
+        }
     }
 }
