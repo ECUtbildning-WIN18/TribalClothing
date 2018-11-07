d3186762d8e712ef32f6e733c9114604f3aed8fb
unknown <iperish.x@gmail.com>
2018-11-07 11:07:47 +0100

    Add initial migration

--- TribalClothing.ProductImporter/TribalClothingContext.cs
+++ TribalClothing.ProductImporter/TribalClothingContext.cs
@@ -7,7 +7,7 @@ namespace TribalClothing.ProductImporter
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer("Server=.;Database=TribalClothing;Integrated Security=True;");
+            optionsBuilder.UseSqlServer("Server=127.0.0.1;Database=TribalClothing;user id=SA; pwd=Ryan2134!");
         }
 
         public DbSet<Product> Products { get; set; }
