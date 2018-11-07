0c097bbb7501c523dbcb6058847123bd8a8d3701
Nayomi Arvell <nayomi.arvell@gmail.com>
2018-11-07 12:54:41 +0100

    Updates import csv view

--- TribalClothing.ProductImporter/Views/ImportCsvView.cs
+++ TribalClothing.ProductImporter/Views/ImportCsvView.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using TribalClothing.ProductImporter.Domain;
 
 namespace TribalClothing.ProductImporter.Views
 {
-    class ImportCsvView
+     class ImportCsvView
     {
+        public List<Product> productList = new List<Product>();
+
         public void Display()
         {
+            var lines = File.ReadAllLines("TribalClothingCsv.csv");
 
+            foreach (var product in lines)
+            {
+                var values = product.Split(';');
+
+
+                Product newProduct = new Product();
+                newProduct.Name = values[0];
+                newProduct.Description = values[1];
+                newProduct.Price = decimal.Parse(values[2]);
+                productList.Add(newProduct);
+
+                //productList.Add(new Product()
+                //{
+                //    Name = values[0],
+                //    Description = values[2],
+                //    Price = decimal.Parse(values[3])
+                //});
+
+                Console.WriteLine(newProduct.Name, newProduct.Description, newProduct.Price);
+            } 
         }
     }
 }
+
+
+
