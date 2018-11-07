df848de10d78d5164330a9a8a6c42daf41ffcd81
tokas85 <tomas.lindqvist85@gmail.com>
2018-11-07 09:51:22 +0100

    Add basic menu

---
+++ TribalClothing.ProductImporter/View/MenuView.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TribalClothing.ProductImporter.View
+{
+    class MenuView
+    {
+        public void Display()
+        {
+            Console.WriteLine("1. List products");
+            Console.WriteLine("2. Import from CSV");
+            Console.WriteLine("3. Import from JSON");
+            Console.WriteLine("4. Clear product catalog");
+
+            var menuInputLoop = true;
+            while (menuInputLoop)
+            {
+                var menuInputKey = Console.ReadKey();
+                Console.WriteLine();
+                switch (menuInputKey.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.Clear();
+                        //userSearchView.Display();
+                        menuInputLoop = false;
+                        break;
+
+                    case ConsoleKey.D2:
+                        Console.Clear();
+                        //adminAddUserViewDB.Display();
+                        menuInputLoop = false;
+                        break;
+
+                    case ConsoleKey.D3:
+                        Console.Clear();
+                        //adminDeleteUserViewDB.Display();
+                        menuInputLoop = false;
+                        break;
+
+                    case ConsoleKey.D4:
+                        Console.Clear();
+                        //loginView.Display();
+                        menuInputLoop = false;
+                        break;
+
+                    default:
+                        Console.WriteLine("Wrong input!");
+                        Console.Write("Try again: ");
+                        break;
+                }
+            }
+        }
+    }
+}
