fa12097f281753cc67b6726812d202356b3648ff
ZokiSama <joakim.hall1@gmail.com>
2018-11-07 11:16:29 +0100

    Put menu in a Loop so the program continues after an option is made

--- TribalClothing.ProductImporter/Views/MainMenuView.cs
+++ TribalClothing.ProductImporter/Views/MainMenuView.cs
@@ -8,46 +8,57 @@ namespace TribalClothing.ProductImporter.Views
     {
         public void Display()
         {
-            Console.WriteLine("1. List products");
-            Console.WriteLine("2. Import from CSV");
-            Console.WriteLine("3. Import from JSON");
-            Console.WriteLine("4. Clear product catalog \n");
-            Console.Write("> ");
-
-            var key = ConsoleKey.NoName;
-
-            while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key !=ConsoleKey.D3 && key != ConsoleKey.D4 && key != ConsoleKey.Escape)
+            while (true)
             {
-                key = Console.ReadKey(true).Key;
-            }
+                Console.Clear();
+                Console.WriteLine("1. List products");
+                Console.WriteLine("2. Import from CSV");
+                Console.WriteLine("3. Import from JSON");
+                Console.WriteLine("4. Clear product catalog \n");
+                Console.WriteLine("5. Quit");
+                Console.Write("> ");
 
-            switch (key)
-            {
-                case ConsoleKey.D1:
+                var key = ConsoleKey.NoName;
+
+                while (key != ConsoleKey.D1 && key != ConsoleKey.D2 && key != ConsoleKey.D3 && key != ConsoleKey.D4 &&
+                       key != ConsoleKey.D5 && key != ConsoleKey.Escape)
+                {
+                    key = Console.ReadKey(true).Key;
+                }
+
+
+                switch (key)
+                {
+                    case ConsoleKey.D1:
                     {
                         var products = new ListProductsView();
                         products.Display();
                         break;
                     }
-                case ConsoleKey.D2:
+                    case ConsoleKey.D2:
                     {
                         var csvImport = new ImportCsvView();
                         csvImport.Display();
                         break;
                     }
-                case ConsoleKey.D3:
+                    case ConsoleKey.D3:
                     {
                         var jsonImport = new ImportJsonView();
                         jsonImport.Display();
                         break;
                     }
-                case ConsoleKey.D4:
+                    case ConsoleKey.D4:
                     {
                         //insert view
                         break;
                     }
+                    case ConsoleKey.D5:
+                    {
+                        Environment.Exit(0);
+                        break;
+                    }
+                }
             }
         }
-
     }
 }
