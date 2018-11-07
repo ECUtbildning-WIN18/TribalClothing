041e78f2b3696291c569631d3301f6f60964e155
Paula Hernandez <paula.frost@hotmail.com>
2018-11-07 10:34:52 +0100

    Add ListProduct view

--- TribalClothing.ProductImporter/View/ListProductView.cs
+++ TribalClothing.ProductImporter/View/ListProductView.cs
@@ -1,7 +1,34 @@
-﻿namespace TribalClothing.ProductImporter.View
+﻿using System;
+using System.Linq;
+using Remotion.Linq.Parsing.Structure.IntermediateModel;
+
+namespace TribalClothing.ProductImporter.View
 {
     class ListProductView
     {
+        private int count = 0;
 
+        public void Display()
+        {
+            Console.Clear();
+            Console.Write($"Total products: {count}");
+            Console.WriteLine("\nPRODUCT NAME         PRODUCT DESCRIPTION");
+            ListProduct();
+
+            Console.ReadKey();
+        }
+
+        public void ListProduct()
+        {
+            using (var db = new TribalClothingContext())
+            {
+                var productList = db.Products.ToList();
+
+                foreach (var product in productList)
+                {
+                    Console.WriteLine($"{product.Name}          {product.Description}");   
+                }
+            }
+        }
     }
 }
