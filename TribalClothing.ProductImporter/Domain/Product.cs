6adf9fcc12bd230d80dae6d2319fcdaa5f9038e5
Marielle Redinge <marielleredinge@gmail.com>
2018-11-07 12:26:20 +0100

    add.csv

--- TribalClothing.ProductImporter/Domain/Product.cs
+++ TribalClothing.ProductImporter/Domain/Product.cs
@@ -7,5 +7,17 @@ namespace TribalClothing.ProductImporter.Domain
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
+
+        public Product()
+        {
+
+        }
+
+        public Product(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
     }
 }
