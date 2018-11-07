392c0a8315ace7c871b9db655a9b72e285a2e776
Stefan Lindahl <stefan.lindahl87@gmail.com>
2018-11-07 11:06:00 +0100

    Add LoadJson

--- TribalClothing.ProductImporter/Domain/Product.cs
+++ TribalClothing.ProductImporter/Domain/Product.cs
@@ -1,9 +1,12 @@
 ﻿
+using System.Runtime.Serialization;
+
 namespace TribalClothing.ProductImporter.Domain
 {
     class Product
     {
-        public int Id { get; set; }
+        [IgnoreDataMember]
+        public int Id { get; set; } 
         public string Name { get; set; }
         public string Description { get; set; }
         public decimal Price { get; set; }
