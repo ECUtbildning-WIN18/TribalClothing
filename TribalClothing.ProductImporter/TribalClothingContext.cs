d2fc602d2cdc25c7d8c86f62ccee0de1a87cb9fa
Robert Tublen <robert.tublen@junction7.se>
2018-11-07 06:29:58 +0100

    Add Product domain model and TribalClothing DBContext

---
+++ TribalClothing.ProductImporter/TribalClothingContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TribalClothing.ProductImporter.Domain;
+
+namespace TribalClothing.ProductImporter
+{
+    class TribalClothingContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=.;Database=TribalClothing;Integrated Security=True;");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
