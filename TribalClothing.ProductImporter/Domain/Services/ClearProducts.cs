856c2694c1e912091fc7a36ed8e0ff750198ef37
AndersPamp <anders_pamp@hotmail.com>
2018-11-07 09:40:29 +0100

    Add main menu and classes to project

---
+++ TribalClothing.ProductImporter/Domain/Services/ClearProducts.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TribalClothing.ProductImporter.Domain.Services
+{
+    class ClearProducts
+    {
+    }
+}
---
+++ TribalClothing.ProductImporter/Views/MainMenu.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace TribalClothing.ProductImporter.Views
+{
+    class MainMenu
+    {
+        private bool done = false;
+        public void Display()
+        {
+            do
+            {
+                Console.Clear();
+                Console.WriteLine(" # Main menu\n");
+                Console.WriteLine(" 1. List products");
+                Console.WriteLine(" 2. Import from CSV");
+                Console.WriteLine(" 3. Import from JSON");
+                Console.WriteLine(" 4. Clear product catalog");
+                Console.WriteLine(" 5. Exit");
+                Console.Write(" > ");
+
+                var choice = Console.ReadKey(true);
+
+                if (choice.Key == ConsoleKey.D1)
+                {
+                    var manageUserView = new ManageUserView();
+                    manageUserView.Display();
+                }
+                else if (choice.Key == ConsoleKey.D2)
+                {
+                    var loginView = new LoginView();
+                    loginView.Display();
+                    done = true;
+                }
+                else if (choice.Key == ConsoleKey.D3 || choice.Key == ConsoleKey.NumPad3)
+                {
+
+                }
+                else if (choice.Key == ConsoleKey.D4)
+                {
+                    
+                }
+                else if (choice.Key == ConsoleKey.D5)
+                {
+                    Console.Write("Exiting.");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Thread.Sleep(250); Console.Write(".");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input, try again");
+                    Thread.Sleep(750);
+                }
+            } while (!done);
+        }
+    }
+}
