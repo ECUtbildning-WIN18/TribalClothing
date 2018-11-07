970d6fb611f9102b17c4e9ec0d0e45df4851dd4d
Joakim Persson <Joakiimm@hotmail.com>
2018-11-07 09:36:19 +0100

    add mainview

--- TribalClothing.ProductImporter/Views/MainView.cs
+++ TribalClothing.ProductImporter/Views/MainView.cs
@@ -6,5 +6,50 @@ namespace TribalClothing.ProductImporter.Views
 {
     class MainView
     {
+
+        public void StartView()
+        {
+
+            bool menu = true;
+
+            while (menu)
+            {
+                Console.WriteLine("1. List products");
+                Console.WriteLine("2. Import from CSV");
+                Console.WriteLine("3. Import from JSON");
+                Console.WriteLine("4. Clear product catalog");
+
+                string menuChoise = Console.ReadLine();
+
+
+                switch (menuChoise)
+                {
+                    case "1":
+                        {
+                            break;
+                        }
+                    case "2":
+                        {
+                            break;
+                        }
+                    case "3":
+                        {
+                            break;
+                        }
+                    case "4":
+                        {
+                            break;
+                        }
+
+                    default:
+                        {
+                            break;
+                        }
+                }
+
+            }
+
+        }
+
     }
 }
